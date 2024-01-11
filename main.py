74b336e05c1b3d44c57a069b4b8d610ed03f1bd6
Kanishkarweb <84016004+Kanishkarweb@users.noreply.github.com>
2024-01-11 20:47:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+Tamil = int(input("Enter your Tamil Mark ? "))
+print(Tamil)
+English = int(input("Enter your English Mark ? "))
+print(English)
+Maths = int(input("Enter your Maths Mark ? "))
+print(Maths)
+Science = int(input("Enter your Science Mark ? "))
+print(Science)
+Social = int(input("Enter your Social Mark ? "))
+print(Social)
+
+Total = Tamil + English + Maths + Science + Social
+
+print("Total Marks is ", +Total)
+
+Average = Total / 5
+print("Average is ", +Average, "%")
